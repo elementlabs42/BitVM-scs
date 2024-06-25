e2d9ce565fc2673cea91de2051623f729ba39af8
timesaved12345 <156633894+timesaved12345@users.noreply.github.com>
2024-06-25 15:10:23 +0800

    Added more functions to Bridge and EBTC token

--- src/BtcBridge.sol
+++ src/BtcBridge.sol
@@ -2,11 +2,36 @@
 pragma solidity ^0.8.26;
 
 import "./EBTC.sol";
+import "./BtcTxVerifier.sol";
 
 contract BtcBridge {
-  EBTC ebtc;
+    event VerifyPeginDone();
 
-  constructor (EBTC _ebtc) {
-    ebtc = _ebtc;
-  }
+    EBTC ebtc;
+    BtcTxVerifier btcTxVerifier;
+
+    constructor(EBTC _ebtc, BtcTxVerifier _btcTxVerifier) {
+        ebtc = _ebtc;
+        btcTxVerifier = _btcTxVerifier;
+    }
+
+    function verifyPegin(
+        uint256 minConfirmations,
+        uint256 blockNum,
+        BtcTxProof calldata proof,
+        uint256 txOutIx,
+        bytes20 destScriptHash,
+        uint256 amountSats
+    ) external {
+        btcTxVerifier.verifyPayment(minConfirmations, blockNum, proof, txOutIx, destScriptHash, amountSats);
+
+        // TODO: extract addresss from proof and sent the amount to the address
+        // ebtc.mint(extractAddress(proof), amountSats);
+
+        emit VerifyPeginDone();
+    }
+
+    // TODO: implement this function to extract address from proof
+    // function extractAddress(BtcTxProof calldata proof) internal returns (address) {
+    // }
 }
--- src/EBTC.sol
+++ src/EBTC.sol
@@ -4,5 +4,27 @@ pragma solidity ^0.8.26;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract EBTC is ERC20 {
-  constructor() ERC20("eBTC", "ebtc") {}
+    error BridgeOnly();
+
+    event TokenMinted(address to, uint amount);
+
+    address bridge;
+
+    constructor(address _bridge) ERC20("eBTC", "ebtc") {
+        bridge = _bridge;
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 8;
+    }
+
+    function mint(address to, uint256 amount) external {
+        if (msg.sender != bridge) {
+            revert BridgeOnly();
+        }
+
+        _mint(to, amount);
+
+        emit TokenMinted(to, amount);
+    }
 }
